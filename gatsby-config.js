8f01f5e388890adb07e3dbd8bb2a4fff9dfc4477
twilightscapes <ihatespam@gmail.com>
2020-09-25 05:01:57 -0500

    turn simon back on

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,14 @@ module.exports = {
   plugins: [
 
 // Simon G API Importer
-    // {
-    //   resolve: `gatsby-source-plugin-simong`,
-    //   options: {
-    //     spaceId: "123",
-    //     preview: true,
-    //     cacheResponse: false,
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-plugin-simong`,
+      options: {
+        spaceId: "123",
+        preview: true,
+        cacheResponse: false,
+      },
+    },
 // Simon G API Importer
 
     {
